32e55bc5f6f58967c39bcfa7bd229bdacc98579c
AnitaPr <anitabllaca88@gmail.com>
2022-03-09 21:14:45 -0500

    Add in name game

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,13 @@
 class ParamsController < ApplicationController
+  def query_params
+    input_name = params["name"].upcase
+    
+
+    if input_name.upcase.first == "A"
+     output_message = "Hey, your name starts with the first letter of the alphabet!"
+    else
+    output_message = "Cool!"
+    end
+    render json: {message: "your name is #{input_name}. #{output_message}"}
+ end
 end
